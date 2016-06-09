2c508f1b9f1658e56337df199729956d7af9dd95
Vinicius <dragilusgames@gmail.com>
2016-06-09 12:37:00 -0300

    Criação de classe para controle de dados de Pessoa(model) con suas regras de negocio. Refs T94

---
+++ src/academia/control/ControlePessoa.java
@@ -0,0 +1,37 @@
+package academia.control;
+
+import academia.HibernateUtil;
+import academia.model.Endereco;
+import academia.model.Pessoa;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author vinicius
+ */
+public class ControlePessoa {
+
+    public static void criar(Pessoa p, Endereco endereco) {
+        Endereco.enderecoDAO().save(endereco);
+        p.setEndereco(endereco);
+        Pessoa.pessoaDAO().save(p);
+
+    }
+    
+    public static Pessoa buscarCPF(String cpf){
+     Criteria crit = HibernateUtil.getSessionFactory().openSession().createCriteria(Pessoa.class);
+        crit.add(Restrictions.eq("cpf", cpf));
+        Pessoa p = Pessoa.pessoaDAO().getEntityByCriteria(crit);
+        return p;
+        
+    }
+    
+    public static Pessoa buscar(String cpf, int tipo) {
+        Criteria crit = HibernateUtil.getSessionFactory().openSession().createCriteria(Pessoa.class);
+        crit.add(Restrictions.eq("cpf", cpf)).add(Restrictions.eq("tipo", tipo));
+        Pessoa p = Pessoa.pessoaDAO().getEntityByCriteria(crit);
+        return p;
+    }
+
+}
