0307a544924dc95d08836e1aee44856582644fad
Vinicius <dragilusgames@gmail.com>
2016-06-17 14:15:27 -0300

    Criação da Classe com os dados do cliente. Refs T138

--- src/academia/control/ControleTelas.java
+++ src/academia/control/ControleTelas.java
@@ -5,37 +5,40 @@
  */
 package academia.control;
 
-import academia.view.Painel;
 import academia.view.TelaFuncionairo;
 import academia.view.TelaInstrutor;
-import java.awt.Dimension;
 
 /**
  *
  * @author vinicius
  */
-public class ControleTelas extends Thread{
+public class ControleTelas extends Thread {
+
     TelaFuncionairo tela;
     TelaInstrutor telaI;
+
     public ControleTelas(TelaFuncionairo tela) {
-    this.tela=tela;
+        this.tela = tela;
     }
+
     public ControleTelas(TelaInstrutor telaI) {
-    this.telaI=telaI;
+        this.telaI = telaI;
     }
-    
+
     public void run() {
 
-     while(true)
-                {
-			if(telaI==null){tela.getPainel().atualizarTamanho(tela.getSize());}
-                        else{telaI.getPainel().atualizarTamanho(telaI.getSize());}
-			try {
-				Thread.sleep(30);
-                                } catch (InterruptedException e) {e.printStackTrace();}
-		}
-			
-     
-     
-     }
+        while (true) {
+            if (telaI == null) {
+                tela.getPainel().atualizarTamanho(tela.getSize());
+            } else {
+                telaI.getPainel().atualizarTamanho(telaI.getSize());
+            }
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
